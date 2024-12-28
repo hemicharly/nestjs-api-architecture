a568ed7cdbfde10723d5181044873dddf09d508a
hemicharly <hemicharlythiago@gmail.com>
2024-12-28 11:12:47 -0300

    feat: add health check

--- app/src/application/web/controllers/health/health.controller.ts
+++ app/src/application/web/controllers/health/health.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ErrorResponse } from '@application/web/response/error/error.response';
+import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { ApiDocGenericGet } from '@application/web/config/swagger/decorators';
+import { HealthCheckResultResponse } from '@application/web/response/health';
+import { HealthCheckResultMapper } from '@application/web/mappers/health/health.check.result.mapper';
 
 @ApiTags('Health')
 @ApiResponse({
@@ -8,16 +12,24 @@ import { ErrorResponse } from '@application/web/response/error/error.response';
   description: 'Internal server error',
   type: ErrorResponse,
 })
-@Controller('/health')
+@Controller('health')
 export class HealthController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly dbHealthIndicator: TypeOrmHealthIndicator,
+  ) {}
+
   @Get()
   @HttpCode(200)
-  @ApiOkResponse({ description: 'Health was successful.', type: String })
-  @ApiOperation({
-    summary: 'Returns the health of the API.',
-    description: 'Returns the health of the API.',
-  })
-  getHealth(): string {
-    return 'OK';
+  @ApiDocGenericGet('health', HealthCheckResultResponse)
+  @HealthCheck()
+  async check(): Promise<HealthCheckResultResponse> {
+    const healthCheckResult = await this.healthCheckService.check([async () => this.dbHealthIndicator.pingCheck('database', { timeout: 3000 })]);
+    const info = { ...healthCheckResult.info };
+    info['application'] = {
+      status: 'up',
+      uptime: process.uptime(),
+    };
+    return HealthCheckResultMapper.toApi({ ...healthCheckResult, info: info });
   }
 }
