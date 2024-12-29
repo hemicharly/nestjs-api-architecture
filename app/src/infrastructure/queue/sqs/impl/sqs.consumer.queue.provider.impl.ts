f97c85ca34929d285424ef3ebf10e2ce123a1c56
hemicharly <hemicharlythiago@gmail.com>
2024-12-29 19:59:50 -0300

    feat: add decorator to sqs

--- app/src/infrastructure/queue/sqs/impl/sqs.consumer.queue.provider.impl.ts
+++ app/src/infrastructure/queue/sqs/impl/sqs.consumer.queue.provider.impl.ts
@@ -10,10 +10,30 @@ import { TracerContextAudit } from '@shared/audit';
 @Injectable()
 export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(SqsConsumerQueueProviderImpl.name);
+
+  /**
+   * AWS SQS client used for interacting with queues.
+   */
   private readonly sqsClient: SQSClient;
+
+  /**
+   * A map of registered queue handlers.
+   * - Key: Queue configuration (e.g., queueName, batchSize).
+   * - Value: The handler function to process messages.
+   */
   private readonly handlers = new Map<SqsDecoratorsTypes, Function>();
+
+  /**
+   * A map of polling intervals for each queue.
+   * - Key: Queue URL.
+   * - Value: NodeJS timeout object for the polling process.
+   */
   private readonly pollingIntervals = new Map<string, NodeJS.Timeout>();
-  private isShuttingDown = false;
+
+  /**
+   * Flag to indicate if the application is shutting down.
+   */
+  private isShuttingDown: boolean = false;
 
   constructor(
     private readonly discoveryService: DiscoveryService,
@@ -29,20 +49,34 @@ export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestr
     this.sqsClient = new SQSClient(config);
   }
 
+  /**
+   * Lifecycle hook triggered when the module is initialized.
+   * - Registers message handlers.
+   * - Starts the polling process for SQS messages.
+   */
   async onModuleInit() {
     this.logger.log('SQS Consumer initialized.');
     this.registerHandlers();
     this.startPolling();
   }
 
+  /**
+   * Lifecycle hook triggered when the module is destroyed.
+   * - Stops all polling processes.
+   * - Marks the consumer as shutting down.
+   */
   async onModuleDestroy() {
     this.isShuttingDown = true;
-    this.logger.log('Shutting down sqs consumer...');
+    this.logger.log('Shutting down SQS Consumer...');
     this.stopAllPolling();
     await Promise.all([...this.pollingIntervals.values()].map((interval) => clearTimeout(interval)));
     this.logger.log('All polling operations stopped.');
   }
 
+  /**
+   * Registers all available handlers for SQS queues.
+   * - Scans through discovered providers and extracts methods annotated with specific metadata.
+   */
   private registerHandlers() {
     const providers = this.discoveryService.getProviders();
     for (const provider of providers) {
@@ -58,29 +92,23 @@ export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestr
         const metadata = Reflect.getMetadata(SQS_HANDLER_METADATA, method);
         if (metadata) {
           const { queueName, batchSize, waitTimeSeconds, visibilityTimeout, enabledVisibilityTimeout } = metadata;
-          this.handlers.set(
-            {
-              queueName,
-              batchSize,
-              waitTimeSeconds,
-              visibilityTimeout,
-              enabledVisibilityTimeout,
-            },
-            method.bind(instance),
-          );
+          this.handlers.set({ queueName, batchSize, waitTimeSeconds, visibilityTimeout, enabledVisibilityTimeout }, method.bind(instance));
         }
       }
     }
   }
 
+  /**
+   * Starts polling for messages from all registered SQS queues.
+   * - Implements exponential backoff in case of errors.
+   */
   private startPolling(): void {
     let backoffTime = 1000; // Start with 1s
-    const maxBackoffTime = 30000; // Time max of 30s
+    const maxBackoffTime = 30000; // Maximum backoff time is 30s
 
     const poll = async () => {
-      if (this.isShuttingDown) {
-        return;
-      }
+      if (this.isShuttingDown) return;
+
       for (const [{ queueName, batchSize, waitTimeSeconds, visibilityTimeout, enabledVisibilityTimeout }, handler] of this.handlers.entries()) {
         const queueUrl = configEnv.aws.sqs.queueUrl(queueName);
         try {
@@ -127,6 +155,11 @@ export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestr
     poll();
   }
 
+  /**
+   * Deletes a processed message from the SQS queue.
+   * @param queueUrl - The URL of the SQS queue.
+   * @param receiptHandle - The receipt handle of the message.
+   */
   private async deleteMessage(queueUrl: string, receiptHandle: string) {
     if (!receiptHandle) {
       this.logger.warn(`[QueueUrl: ${queueUrl}] Invalid receipt handle for queue.`);
@@ -145,6 +178,12 @@ export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestr
     }
   }
 
+  /**
+   * Extends the visibility timeout for a message in the SQS queue.
+   * @param queueUrl - The URL of the SQS queue.
+   * @param receiptHandle - The receipt handle of the message.
+   * @param visibilityTimeout - The new visibility timeout in seconds.
+   */
   private async extendVisibilityTimeout(queueUrl: string, receiptHandle: string, visibilityTimeout: number) {
     if (!receiptHandle) {
       this.logger.warn(`[QueueUrl: ${queueUrl}] Invalid receipt handle for queue.`);
@@ -164,6 +203,10 @@ export class SqsConsumerQueueProviderImpl implements OnModuleInit, OnModuleDestr
     }
   }
 
+  /**
+   * Stops all ongoing polling processes.
+   * - Clears all registered intervals for polling.
+   */
   private stopAllPolling() {
     for (const [url, interval] of this.pollingIntervals) {
       clearTimeout(interval);
