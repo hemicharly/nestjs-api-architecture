eb83106c5230ce2bcdfd4208a5dffcb5938bf95a
hemicharly <hemicharlythiago@gmail.com>
2024-12-31 19:39:09 -0300

    chore: add boundaries plugin

--- app/eslint.config.cjs
+++ app/eslint.config.cjs
@@ -1,6 +1,9 @@
 const tsParser = require('@typescript-eslint/parser');
 const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
 const prettierPlugin = require('eslint-plugin-prettier');
+const importPlugin = require('eslint-plugin-import');
+const boundariesPlugin = require('eslint-plugin-boundaries');
+
 
 module.exports = [
   {
@@ -19,7 +22,22 @@ module.exports = [
     },
     plugins: {
       '@typescript-eslint': typescriptPlugin,
-      prettier: prettierPlugin
+      prettier: prettierPlugin,
+      'import': importPlugin,
+      boundaries: boundariesPlugin
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      },
+      'boundaries/elements': [
+        { type: 'core', pattern: 'src/core/**' },
+        { type: 'entrypoints', pattern: 'src/entrypoints/**' },
+        { type: 'infrastructure', pattern: 'src/infrastructure/**' },
+        { type: 'shared', pattern: 'src/shared/**' }
+      ],
     },
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
@@ -32,7 +50,33 @@ module.exports = [
         {
           endOfLine: 'auto'
         }
-      ]
+      ],
+      "boundaries/element-types": [2, {
+        default: "disallow",
+        message: "The '${file.type}' layer is not allowed to import the '${dependency.type}'. Because it violates the principle of responsibility.",
+        rules: [
+          {
+            from: ["entrypoints"],
+            allow: ["entrypoints", "infrastructure", "core", "shared"],
+            importKind: "value",
+          },
+          {
+            from: ["core"],
+            allow: ["core"],
+            importKind: "value",
+          },
+          {
+            from: ["infrastructure"],
+            allow: ["infrastructure", "core", "shared"],
+            importKind: "value",
+          },
+          {
+            from: ["shared"],
+            allow: ["shared", "core"],
+            importKind: "value",
+          },
+        ]
+      }]
     }
   },
   {
--- app/test/entrypoints/web/rest/orders/orders.controller.spec.ts
+++ app/test/entrypoints/web/rest/orders/orders.controller.spec.ts
@@ -1,27 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiKeyGuard } from '@entrypoints/web/shared/middleware/apikey';
-import {
-  OrderCreationUseCaseImpl,
-  OrderEndUsecaseImpl,
-  OrderFindByIdUsecaseImpl,
-  OrderQueryQuantityStatusUsecaseImpl,
-  OrderQueryUsecaseImpl,
-  OrderStartUsecaseImpl
-} from '@core/usecases/orders/impl';
-import {
-  OrderCreationRequest, OrderEndRequest,
-  OrderQueryQuantityStatusRequest,
-  OrderQueryRequest, OrderStartRequest
-} from '@entrypoints/web/rest/orders/request';
-import {
-  OrderCreateResponse,
-  OrderItemsResponse,
-  OrderPaginationResponse,
-  OrderQuantityStatusResponse
-} from '@entrypoints/web/rest/orders/response';
+import { OrderCreationUseCaseImpl, OrderEndUsecaseImpl, OrderFindByIdUsecaseImpl, OrderQueryQuantityStatusUsecaseImpl, OrderQueryUsecaseImpl, OrderStartUsecaseImpl } from '@core/usecases/orders/impl';
+import { OrderCreationRequest, OrderEndRequest, OrderQueryQuantityStatusRequest, OrderQueryRequest, OrderStartRequest } from '@entrypoints/web/rest/orders/request';
+import { OrderCreateResponse, OrderItemsResponse, OrderPaginationResponse, OrderQuantityStatusResponse } from '@entrypoints/web/rest/orders/response';
 import { OrdersController } from '@entrypoints/web/rest/orders';
 import { OrderStatus } from '@core/domain/enums';
-
 
 describe('OrdersController', () => {
   let controller: OrdersController;
@@ -42,8 +25,8 @@ describe('OrdersController', () => {
         { provide: OrderQueryQuantityStatusUsecaseImpl.name, useValue: mockOrderQueryQuantityStatusUsecase },
         { provide: OrderFindByIdUsecaseImpl.name, useValue: mockOrderFindByIdUsecase },
         { provide: OrderStartUsecaseImpl.name, useValue: mockOrderStartUsecase },
-        { provide: OrderEndUsecaseImpl.name, useValue: mockOrderEndUsecase }
-      ]
+        { provide: OrderEndUsecaseImpl.name, useValue: mockOrderEndUsecase },
+      ],
     })
       .overrideGuard(ApiKeyGuard)
       .useValue({ canActivate: jest.fn(() => true) })
@@ -57,19 +40,19 @@ describe('OrdersController', () => {
   });
 
   it('should call OrderCreationUseCase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
     const mockDto: OrderCreationRequest = {
       serviceDescription: 'BHUT serviço',
       companyName: 'BHUT',
       companyAddressLatitude: -39.932048,
       companyAddressLongitude: 34.664223,
-      schedulingDate: '2024-12-27'
+      schedulingDate: '2024-12-27',
     };
     const mockResponse: OrderCreateResponse = {
       id: '6773205c93b1daa335239f03',
       employeeId: 'a780e1d0-0b60-4a0f-b2a2-fda12ecb81af',
       status: 'OPEN',
-      createdAt: '2024-12-30T22:36:12.881Z'
+      createdAt: '2024-12-30T22:36:12.881Z',
     };
     jest.spyOn(mockOrderCreationUseCase, 'execute').mockResolvedValue(mockResponse);
     const result = await controller.createOrder(mockRequest, mockDto);
@@ -78,7 +61,7 @@ describe('OrdersController', () => {
   });
 
   it('should call OrderQueryUsecase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
     const mockDto: OrderQueryRequest = {
       page: 0,
       pageSize: 10,
@@ -88,13 +71,13 @@ describe('OrdersController', () => {
       pagination: {
         page: 0,
         pageSize: 10,
-        total: 0
+        total: 0,
       },
       totalHours: {
         hours: 0,
         minutes: 0,
-        seconds: 0
-      }
+        seconds: 0,
+      },
     };
     jest.spyOn(mockOrderQueryUsecase, 'execute').mockResolvedValue(mockResponse);
     const result = await controller.getFilter(mockRequest, mockDto);
@@ -103,19 +86,21 @@ describe('OrdersController', () => {
   });
 
   it('should call OrderQueryQuantityStatusUsecase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
     const mockDto: OrderQueryQuantityStatusRequest = {
       startDate: '2024-12-01',
-      endDate: '2024-12-15'
+      endDate: '2024-12-15',
     };
-    const mockResponse: OrderQuantityStatusResponse[] = [{
-      status: OrderStatus.OPEN,
-      day: 1,
-      month: 12,
-      week: 1,
-      year: 2024,
-      quantity: 10
-    }];
+    const mockResponse: OrderQuantityStatusResponse[] = [
+      {
+        status: OrderStatus.OPEN,
+        day: 1,
+        month: 12,
+        week: 1,
+        year: 2024,
+        quantity: 10,
+      },
+    ];
     jest.spyOn(mockOrderQueryQuantityStatusUsecase, 'execute').mockResolvedValue(mockResponse);
     const result = await controller.getQuantityStatus(mockRequest, mockDto);
     expect(mockOrderQueryQuantityStatusUsecase.execute).toHaveBeenCalledWith(expect.anything());
@@ -123,8 +108,8 @@ describe('OrdersController', () => {
   });
 
   it('should call OrderFindByIdUsecase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
-    const mockDto: string = "6773205c93b1daa335239f03";
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
+    const mockDto: string = '6773205c93b1daa335239f03';
     const mockResponse: OrderItemsResponse = {
       companyName: 'Company',
       createdAt: new Date().toJSON(),
@@ -133,7 +118,7 @@ describe('OrdersController', () => {
       schedulingDate: '2024-12-10',
       serviceDescription: 'Description',
       status: OrderStatus.OPEN,
-      updatedAt: new Date().toJSON()
+      updatedAt: new Date().toJSON(),
     };
     jest.spyOn(mockOrderFindByIdUsecase, 'execute').mockResolvedValue(mockResponse);
     const result = await controller.getOrderById(mockRequest, mockDto);
@@ -142,26 +127,25 @@ describe('OrdersController', () => {
   });
 
   it('should call OrderStartUsecase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
     const mockDto: OrderStartRequest = {
-      startDatetime: "2024-12-29T22:59:41.255Z",
+      startDatetime: '2024-12-29T22:59:41.255Z',
       recordedLatitude: 13.174342,
-      recordedLongitude: 101.215167
-    }
+      recordedLongitude: 101.215167,
+    };
     await controller.startOrder(mockRequest, '6773205c93b1daa335239f03', mockDto);
     expect(mockOrderStartUsecase.execute).toHaveBeenCalledWith(expect.anything());
   });
 
   it('should call OrderEndUsecase.execute with correct parameters', async () => {
-    const mockRequest = { headers: {'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d'}, body: {} } as any;
+    const mockRequest = { headers: { 'x-api-key': '8c2e7d525bd34adfb52bb1fb24357a2d' }, body: {} } as any;
     const mockDto: OrderEndRequest = {
-      endDatetime: "2024-12-29T23:59:41.255Z",
+      endDatetime: '2024-12-29T23:59:41.255Z',
       recordedLatitude: 13.174342,
       recordedLongitude: 101.215167,
-      comment: 'Finished'
-    }
+      comment: 'Finished',
+    };
     await controller.endOrder(mockRequest, '6773205c93b1daa335239f03', mockDto);
     expect(mockOrderEndUsecase.execute).toHaveBeenCalledWith(expect.anything());
   });
-
 });
