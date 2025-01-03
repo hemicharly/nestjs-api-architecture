2e67cb1839abab10ea4c4dad2bc49741b5dbe380
hemicharly <hemicharlythiago@gmail.com>
2025-01-03 11:06:46 -0300

    feat: add access denied

--- app/generate-index.js
+++ app/generate-index.js
@@ -1,31 +1,36 @@
-const { execSync } = require('child_process');
-const { writeFile, lstatSync } = require('fs');
+const { writeFile, readdirSync, lstatSync } = require('fs');
 const path = require('path');
 
-const getStagedFiles = () => {
-  try {
-    const output = execSync('git diff --cached --name-only --diff-filter=A').toString();
-    return output.split('\n').filter((file) => file.endsWith('.ts'));
-  } catch (err) {
-    console.error('Error getting staged files:', err.message);
-    return [];
-  }
+const directoryPath = path.join(__dirname, './src');
+
+const getAllFiles = (dirPath, arrayOfFiles) => {
+  arrayOfFiles = arrayOfFiles || [];
+  const files = readdirSync(dirPath);
+
+  files.forEach((file) => {
+    const fullPath = path.join(dirPath, file);
+    if (lstatSync(fullPath).isDirectory()) {
+      arrayOfFiles = getAllFiles(fullPath, arrayOfFiles);
+    } else if (file.endsWith('.ts')) {
+      arrayOfFiles.push(fullPath);
+    }
+  });
+
+  return arrayOfFiles;
 };
 
-const generateIndexFilesForNewFiles = (stagedFiles) => {
-  const filesByDir = stagedFiles.reduce((acc, file) => {
-    const fullPath = path.resolve(file);
-    const dir = path.dirname(fullPath);
+const generateIndexFiles = (baseDir) => {
+  const allFiles = getAllFiles(baseDir);
 
-    if (!lstatSync(fullPath).isDirectory()) {
-      if (!acc[dir]) {
-        acc[dir] = [];
-      }
-      acc[dir].push(fullPath);
+  const filesByDir = allFiles.reduce((acc, file) => {
+    const dir = path.dirname(file);
+    if (!acc[dir]) {
+      acc[dir] = [];
     }
-
+    acc[dir].push(file);
     return acc;
   }, {});
+
 
   Object.entries(filesByDir).forEach(([dir, files]) => {
     const exportStatements = files
@@ -48,11 +53,4 @@ const generateIndexFilesForNewFiles = (stagedFiles) => {
   });
 };
 
-// Main execution
-const stagedFiles = getStagedFiles();
-if (stagedFiles.length > 0) {
-  console.log('Generating index files for newly added files...');
-  generateIndexFilesForNewFiles(stagedFiles);
-} else {
-  console.log('No new TypeScript files to process.');
-}
+generateIndexFiles(directoryPath);
--- app/src/core/domain/exceptions/index.ts
+++ app/src/core/domain/exceptions/index.ts
@@ -1,1 +1,6 @@
 export * from './custom-access-denied.exception';
+export * from './custom-base.exception';
+export * from './custom-business.exception';
+export * from './custom-conflict.exception';
+export * from './custom-resource-not-found.exception';
+export * from './custom-unauthorized.exception';
